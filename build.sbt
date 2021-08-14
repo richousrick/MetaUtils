cef53e99ca1a0f036a96868b8162ef97b6126e6f
Rikkey <rikkeypaal@gmail.com>
2021-08-14 08:53:09 +0100

    Updated to scala 3.0.1

--- build.sbt
+++ build.sbt
@@ -1,8 +1,7 @@
 name := "MetaUtils"
 
-version := "0.1"
+version := "1.0.1"
 
-scalaVersion := "2.13.4"
+scalaVersion := "3.0.1"
 
-libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.2"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.2" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
--- src/test/scala/com/richousrick/metautils/loader/StreamMapperTest.scala
+++ src/test/scala/com/richousrick/metautils/loader/StreamMapperTest.scala
@@ -13,7 +13,7 @@ class StreamMapperTest extends AnyFunSuite {
 
   test("Cannot create streams for duplicate keys") {
     val mapper = new StreamMapper
-    val a = mapper.newStream("Key A").getOrElse(fail)
+    val a = mapper.newStream("Key A").getOrElse(fail())
 
     val aByte = "Hello".getBytes("UTF-8")
     a.write(aByte)
@@ -27,8 +27,8 @@ class StreamMapperTest extends AnyFunSuite {
 
   test("Can read data written to streams") {
     val mapper = new StreamMapper
-    val a = mapper.newStream("Stream 1").getOrElse(fail)
-    val b = mapper.newStream("Stream 2").getOrElse(fail)
+    val a = mapper.newStream("Stream 1").getOrElse(fail())
+    val b = mapper.newStream("Stream 2").getOrElse(fail())
 
     val example1 = "Hello this is an example set of text".getBytes("UTF-8")
     val example2 = "This is a different set of text".getBytes("UTF-8")
@@ -36,22 +36,22 @@ class StreamMapperTest extends AnyFunSuite {
     a.write(example1)
     b.write(example2)
 
-    assert(mapper.files.getOrElse("Stream 1", fail) sameElements example1)
+    assert(mapper.files.getOrElse("Stream 1", fail()) sameElements example1)
 
-    assert(mapper.files.getOrElse("Stream 2", fail) sameElements example2)
+    assert(mapper.files.getOrElse("Stream 2", fail()) sameElements example2)
 
-    assert(!(mapper.files.getOrElse("Stream 1", fail) sameElements mapper.files.getOrElse("Stream 2", fail)))
+    assert(!(mapper.files.getOrElse("Stream 1", fail()) sameElements mapper.files.getOrElse("Stream 2", fail())))
 
 
   }
 
   test("Cannot generate new streams after reading") {
     val mapper = new StreamMapper
-    mapper.newStream("Stream 1").getOrElse(fail)
-    mapper.newStream("Stream 2").getOrElse(fail)
+    mapper.newStream("Stream 1").getOrElse(fail())
+    mapper.newStream("Stream 2").getOrElse(fail())
 
     val mapper2 = new StreamMapper
-    mapper2.newStream("Stream 1").getOrElse(fail)
+    mapper2.newStream("Stream 1").getOrElse(fail())
     mapper2.files
     assert(mapper2.newStream("Stream2").isEmpty)
   }
